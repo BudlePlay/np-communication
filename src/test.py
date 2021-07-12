bbb6d9494bb590354d7f9cee0aca632dbff0986c
Song <217.Songs.SH@gmail.com>
2021-07-12 14:11:13 +0900

    fix end_check logic

--- src/test.py
+++ src/test.py
@@ -2,9 +2,9 @@ import serial
 from collections import deque
 from pynput.keyboard import Key, Controller
 
-ser = serial.Serial("COM4", 115200, timeout = 1)
+ser = serial.Serial("COM7", 115200, timeout=1)
 
-q=deque(maxlen=15)
+q = deque(maxlen=15)
 
 import torch
 
@@ -12,6 +12,7 @@ model = torch.load('model.pth', map_location=torch.device('cpu'))
 model.eval()
 
 gesture_dict = {0: 'w', 1: 'swing', 2: 'poke', 3: '', 4: ''}
+
 
 class KeyboardInput:
     def __init__(self):
@@ -39,42 +40,41 @@ class KeyboardInput:
     def typeString(self, string):
         self.keyboard.type(string)
 
-kb=KeyboardInput()
+
+kb = KeyboardInput()
+
 
 class CMemory:
     def __init__(self):
-        self.q=deque()
-        self.can_add=False
+        self.q = deque()
+        self.can_add = False
+
     def add(self, data):
         if self.can_add:
             self.q.append(data)
 
     def allow_add(self):
-        self.can_add=True
+        self.can_add = True
 
     def block_add(self):
-        self.can_add=False
+        self.can_add = False
 
     def end_check(self):
 
-        flag = False
-
         if len(self.q) > 3:
-            print(self.q[-1], self.q[-2], self.q[-3])
-            for i in range(1, 3):
-                # print(self.q[-1 * i])
-                
-                if self.q[-1 * i] == 0:
-                    flag = True
-        
-        if flag == True:
-            self.block_add()
-        return flag
+            for i in range(1, 4):
+                if self.q[-1 * i] != 0:
+                    return False
+        else:
+            return False
+
+        self.block_add()
+        return True
 
 
-prev_gesture=0
+prev_gesture = 0
 
-memory=CMemory()
+memory = CMemory()
 while True:
     data = ser.readline().decode('utf-8').strip()
     data = data.split(',')
@@ -83,7 +83,7 @@ while True:
         data[i] = float(data[i])
 
     q.append(data)
-    if len(q)>=15:
+    if len(q) >= 15:
         input_tenser = torch.tensor(q)
 
         accel = input_tenser[:, 0:3]
@@ -94,17 +94,16 @@ while True:
         out = model(accel.unsqueeze(0), gyro.unsqueeze(0))
         gesture = out.argmax().item()
 
-        if prev_gesture!=gesture and gesture!=0:
+        if prev_gesture != gesture and gesture != 0:
             memory.allow_add()
 
         memory.add(gesture)
-        
+
         if memory.end_check():
             print(memory.q)
             memory.q.clear()
-            
+
         # print(gesture_dict[gesture])
 
-        prev_gesture=gesture
+        prev_gesture = gesture
         # kb.inputKey('J')
-
